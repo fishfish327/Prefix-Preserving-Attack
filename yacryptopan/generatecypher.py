b2c40859ed9b4c758d84dae3e384608de4874845
shuai <shuaitong963@gmail.com>
2020-05-07 00:02:57 -0500

    init

---
+++ yacryptopan/generatecypher.py
@@ -0,0 +1,10 @@
+#!/bin/python2
+from yacryptopan import CryptoPAn
+FILE="IP.csv"
+cp = CryptoPAn('32-char-str-for-AES-key-and-pad.')
+with open(FILE, "r") as input:
+    flines = input.readlines()
+    for plaintext in flines:
+        print (cp.anonymize(plaintext))
+    
+
